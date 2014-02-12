4b4a0625f229da021dba1cab97360f2afe8e8d69
Hakan Bilgin <hbi99@hotmail.com>
2014-02-12 09:08:29 +0100

    Objrect.create polyfill

--- junior.js
+++ junior.js
@@ -5,6 +5,21 @@
 (function(root, document) {
     'use strict';
 
+    if (typeof Object.create !== 'function') {
+        Object.create = function(o, props) {
+            function F() {}
+            F.prototype = o;
+            if (typeof(props) === "object") {
+                for (prop in props) {
+                    if (props.hasOwnProperty((prop))) {
+                        F[prop] = props[prop];
+                    }
+                }
+            }
+            return new F();
+        };
+    }
+
     var Junior = function() {
         var coll = Object.create(Array.prototype);
         for (var prop in Junior.prototype) {
